96d06e58fedfa3e734f1b0fb636fd068c9a21b9a
Muraoka <ym.animator@gmail.com>
2022-07-10 13:16:20 +0900

    ページネーションのバグを修正

--- parts/pagination.php
+++ parts/pagination.php
@@ -7,7 +7,7 @@
             $listClass .= $paginationSource['current'] == 1 ? ' disabled' : '';
         ?>
         <li class="<?php h($listClass); ?>">
-            <a class="page-link" href="<?php h(get_pagenum_link() . 'page/' . ($paginationSource['current'] - 1)); ?>">Prev</a>
+            <a class="page-link" href="<?php h(get_pagenum_link(($paginationSource['current'] - 1))); ?>">Prev</a>
         </li>
         <?php for ($p = $paginationSource['start']; $p <= $paginationSource['end']; $p++) : ?>
             <?php
@@ -15,7 +15,7 @@
                 $listClass .= $paginationSource['current'] == $p ? ' active' : '';
             ?>
             <li class="<?php h($listClass); ?>">
-                <a class="page-link" href="<?php h(get_pagenum_link() . 'page/' . $p); ?>"><?php h($p); ?></a>
+                <a class="page-link" href="<?php h(get_pagenum_link($p)); ?>"><?php h($p); ?></a>
             </li>
         <?php endfor; ?>
         <?php
@@ -23,20 +23,20 @@
             $listClass .= $paginationSource['current'] == $paginationSource['total'] ? ' disabled' : '';
         ?>
         <li class="<?php h($listClass); ?>">
-            <a class="page-link" href="<?php h(get_pagenum_link() . 'page/' . ($paginationSource['current'] + 1)); ?>">Next</a>
+            <a class="page-link" href="<?php h(get_pagenum_link(($paginationSource['current'] + 1))); ?>">Next</a>
         </li>
     </ul>
     <div class="float-start">
         <input class="text-center" id="target-page" type="number" min="1" max="<?php h($paginationSource['total']); ?>" step="1" value="<?php h($paginationSource['current']); ?>">
         / <?php h($paginationSource['total']); ?>
-        <a class="btn btn-primary text-white ms-1" id="target-page-link" href="" data-base-url="<?php h(get_pagenum_link() . 'page/'); ?>">GO</a>
+        <a class="btn btn-primary text-white ms-1" id="target-page-link" href="<?php h(get_pagenum_link(($paginationSource['current']))); ?>">GO</a>
     </div>
 </div>
 <script>
     const pageLink = document.getElementById('target-page-link');
-    const baseUrl = pageLink.getAttribute('data-base-url');
     document.getElementById('target-page').addEventListener('change', (e) => {
-        pageLink.href = baseUrl + e.target.value;
+        const n = 1 + parseInt(e.target.value);
+        pageLink.href = document.querySelector(`.page-item:nth-child(${n}) > a`).href;
     });
 </script>
 <?php endif; ?>
