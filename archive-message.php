12f19a8e2a748b825850b1f0504ad1e46558b425
Muraoka <ym.animator@gmail.com>
2022-07-24 12:31:07 +0900

    アーカイブ表示

--- archive-message.php
+++ archive-message.php
@@ -16,12 +16,15 @@ get_header();
 <?php if (have_posts()) : ?>
   <div class="container">
     <?php while (have_posts()) : the_post(); ?>
-      <a class="text-decoration-none" href="<?php echo get_the_permalink(); ?>">
+      <a class="d-block mt-4 text-decoration-none" href="<?php echo get_the_permalink(); ?>">
         <dl class="message">
           <dt class="mb-2"><span class="d-inline-block bg-secondary px-2 text-white fw-normal"><?php the_title(); ?></span></dt>
           <dd class="text-dark h4 fw-normal mb-0"><?php the_field('title_ja'); ?></dd>
           <dd class="text-dark h5 fw-normal mt-0"><?php the_field('title_en'); ?></dd>
-          <dd class="text-dark"><?php the_field('messanger'); ?></dd>
+          <dd class="text-dark">
+            <?php echo get_post_meta(get_field('messanger_id'), 'name_ja', true); ?> /
+            <?php echo get_the_title(get_field('messanger_id')); ?>
+          </dd>
         </dl>
       </a>
     <?php endwhile; ?>
